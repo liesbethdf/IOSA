5e6b54a592435653cfba018304766f1382e6d502
liesbethdefosse@gmail.com <liesbethdefosse@gmail.com>
2018-11-14 17:42:23 +0100

    export jobs

--- project/analysis/employmentContent_DomImp.R
+++ project/analysis/employmentContent_DomImp.R
@@ -1,73 +1,5 @@
 
 ############################################
-############ Check Leontief inverse ; ok, same as provided by SA Stat
-############################################
-
-# fd    <- df.IOT2014$Exports[1:n] + df.IOT2014$Household[1:n] + df.IOT2014$General.Government[1:n] + df.IOT2014$Capital.formation[1:n] + df.IOT2014$Changes.in.inventories[1:n]
-# fd.d  <- df.IOT2014.dom$Exports[1:n] + df.IOT2014.dom$Household[1:n] + df.IOT2014.dom$General.Government[1:n] + df.IOT2014.dom$Capital.formation[1:n] + df.IOT2014.dom$Changes.in.inventories[1:n]
-# 
-# s.d   <- fd.d/fd        # share domestic    #fd : final demand #fd.d # final demand met with domestic goods
-
-# Z     <- as.matrix(df.IOT2014[1:n,(1:n)+2])  
-# Z.d   <- as.matrix(df.IOT2014.dom[1:n,(1:n)+3])
-# Z.m   <- as.matrix(df.IOT2014.imp[1:n,(1:n)+3])
-# 
-# A     <- t(t(Z)/df.IOT2014$Output[1:n])
-# A.d   <- t(t(Z.d)/df.IOT2014$Output[1:n])
-# A.m   <- t(t(Z.m)/df.IOT2014$Output[1:n])
-# 
-# L     <- diag(x = 1, nrow=n, ncol = n) - A
-# L.d   <- diag(x = 1, nrow=n, ncol = n) - A.d
-# L.m   <- diag(x = 1, nrow=n, ncol = n) - A.m
-# 
-# LI    <- inv(as.matrix(L))
-# LI.d  <- inv(as.matrix(L.d))
-# LI.m  <- inv(as.matrix(L.m))
-
-# s.d.exp   <- rep(0,n)
-# s.d.cons  <- rep(0,n)
-# s.d.gov   <- rep(0,n)
-# s.d.gfcf  <- rep(0,n)
-# s.d.chi   <- rep(0,n)
-# s.m.exp   <- rep(0,n)
-# s.m.cons  <- rep(0,n)
-# s.m.gov   <- rep(0,n)
-# s.m.gfcf  <- rep(0,n)
-# s.m.chi   <- rep(0,n)
-# 
-# nonzero <- which(!df.IOT2014$Exports[1:50]==0)
-# s.d.exp[nonzero] <- df.IOT2014.dom$Exports[nonzero]/df.IOT2014$Exports[nonzero]
-# 
-# nonzero <- which(!df.IOT2014$Household[1:50]==0)
-# s.d.cons[nonzero] <- df.IOT2014.dom$Household[nonzero]/df.IOT2014$Household[nonzero]
-# 
-# nonzero <- which(!df.IOT2014$General.Government[1:50]==0)
-# s.d.gov[nonzero] <- df.IOT2014.dom$General.Government[nonzero]/df.IOT2014$General.Government[nonzero]
-# 
-# nonzero <- which(!df.IOT2014$Capital.formation[1:50]==0)
-# s.d.gfcf[nonzero] <- df.IOT2014.dom$Capital.formation[nonzero]/df.IOT2014$Capital.formation[nonzero]
-# 
-# nonzero <- which(!df.IOT2014$Changes.in.inventories[1:50]==0)
-# s.d.chi[nonzero] <- df.IOT2014.dom$Changes.in.inventories[nonzero]/df.IOT2014$Changes.in.inventories[nonzero]
-# 
-# nonzero <- which(!df.IOT2014$Exports[1:50]==0)
-# s.m.exp[nonzero] <- df.IOT2014.imp$Exports[nonzero]/df.IOT2014$Exports[nonzero]
-# 
-# nonzero <- which(!df.IOT2014$Household[1:50]==0)
-# s.m.cons[nonzero] <- df.IOT2014.imp$Household[nonzero]/df.IOT2014$Household[nonzero]
-# 
-# nonzero <- which(!df.IOT2014$General.Government[1:50]==0)
-# s.m.gov[nonzero] <- df.IOT2014.imp$General.Government[nonzero]/df.IOT2014$General.Government[nonzero]
-# 
-# nonzero <- which(!df.IOT2014$Capital.formation[1:50]==0)
-# s.m.gfcf[nonzero] <- df.IOT2014.imp$Capital.formation[nonzero]/df.IOT2014$Capital.formation[nonzero]
-# 
-# nonzero <- which(!df.IOT2014$Changes.in.inventories[1:50]==0)
-# s.m.chi[nonzero] <- df.IOT2014.imp$Changes.in.inventories[nonzero]/df.IOT2014$Changes.in.inventories[nonzero]
-# 
-
-
-############################################
 ############ Contenu en emploie / employment content
 ############################################
 
@@ -89,8 +21,8 @@ df.ec.di <- data.frame(employment.content.direct.indirect)
 colnames(df.ec.di) <- SA.IO.codes
 rownames(df.ec.di) <- SA.IO.codes
 
-# df.e.di <- data.frame(employment.content.direct.indirect %*% diag(demand))
-# colnames(df.e.di) <- SA.IO.codes
+ df.e.di <- data.frame(employment.content.direct.indirect %*% diag(demand))
+ colnames(df.e.di) <- SA.IO.codes
 
 df.output.di <- LI.d %*% diag(s.d) %*% diag(demand)
 df.output.di <- add.sectorNames(df.output.di)
